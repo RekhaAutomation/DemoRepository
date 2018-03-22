4117eed6e7746e74011b5e4b894296276b3998ff
Rekha <rekha.ganib@gmail.com>
2018-03-21 21:53:55 -0700

    second commit to selenium link

--- src/main/java/com/qa/pages/HomePage.java
+++ src/main/java/com/qa/pages/HomePage.java
@@ -4,4 +4,7 @@ public class HomePage {
 	public void login() {
 		System.out.println("this is login method"); 
 	}
+	public void setlogin() {
+		System.out.println("this is setlogin method");
+	}
 }
