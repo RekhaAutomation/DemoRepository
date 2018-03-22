a284629c109c0d83548bd082b0a555a8cbe30314
Rekha <rekha.ganib@gmail.com>
2018-03-21 18:11:44 -0700

    First commit for selenium code

---
+++ src/main/java/com/qa/pages/ContactsPage.java
@@ -0,0 +1,8 @@
+package com.qa.pages;
+
+public class ContactsPage {
+	public void selectcontact() {
+		System.out.println("this is select contact method");
+	}
+
+}
---
+++ src/test/java/com/qa/tests/HomepageTest.java
@@ -0,0 +1,11 @@
+package com.qa.tests;
+
+import org.testng.annotations.Test;
+
+public class HomepageTest {
+	@Test 
+	public void validateTitle() {
+		System.out.println("title is matched");
+	}
+
+}
